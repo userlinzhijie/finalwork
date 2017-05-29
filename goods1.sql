1768d921681a24d6d0ffa20128fdd6c5544bdeb6
userlinzhijie <2729192447@qq.com>
2017-05-29 13:04:01 +0800

    数据库更新

--- goods1.sql
+++ goods1.sql
@@ -4,13 +4,13 @@ Navicat MySQL Data Transfer
 Source Server         : 本地
 Source Server Version : 50717
 Source Host           : localhost:3306
-Source Database       : store
+Source Database       : webshop
 
 Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-05-29 00:35:30
+Date: 2017-05-29 13:03:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,6 +21,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
+  `seller_id` int(11) DEFAULT NULL,
   `brand` varchar(255) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
@@ -36,13 +37,14 @@ CREATE TABLE `goods` (
   `picture` varchar(255) DEFAULT NULL,
   `material` varchar(255) DEFAULT NULL,
   `details` varchar(255) DEFAULT NULL,
-  `user` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `seller_goods` (`seller_id`),
+  CONSTRAINT `seller_goods` FOREIGN KEY (`seller_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES ('1', '雷蛇', '189', '雷蛇机械键盘', 'B78', '机械键盘', '黑色', '107键', 'USB', '混色背光', '支持', '441*182*42mm', '1kg左右', null, '金属', '可编程全键无冲', '1');
-INSERT INTO `goods` VALUES ('2', '牧马人', '90', '牧马人鼠标', 'C88', '无线鼠标', '白色', '激光', 'USB&蓝牙', '双色背光', '支持', '13.5*7*4cm', '170g', null, '碳纤维', '可编程可调节配重块', '1');
-INSERT INTO `goods` VALUES ('3', '雷蛇', '34', '激光鼠标', 'T45', '有线鼠标', '亮银', '光电', 'USB', '无', '不支持', '13.5*7.4', '180g', null, '塑料', '宏设置', '2');
+INSERT INTO `goods` VALUES ('1', '2', '雷蛇', '189', '雷蛇机械键盘', 'B78', '机械键盘', '黑色', '107键', 'USB', '混色背光', '支持', '441*182*42mm', '1kg左右', null, '金属', '可编程全键无冲');
+INSERT INTO `goods` VALUES ('2', '4', '牧马人', '90', '牧马人鼠标', 'C88', '无线鼠标', '白色', '激光', 'USB&蓝牙', '双色背光', '支持', '13.5*7*4cm', '170g', null, '碳纤维', '可编程可调节配重块');
+INSERT INTO `goods` VALUES ('3', '1', '雷蛇', '34', '激光鼠标', 'T45', '有线鼠标', '亮银', '光电', 'USB', '无', '不支持', '13.5*7.4', '180g', null, '塑料', '宏设置');
